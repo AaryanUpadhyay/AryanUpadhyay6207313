aed6f3bd84d1c1ef35fc09bddd4c7aaf3e4489c6
AaryanUpadhyay <aaryanupadhyay2803@gmail.com>
2026-03-17 15:53:01 +0530

    Day 21 is uploaded

---
+++ playwright/tests/Day21/task.spec.ts
@@ -0,0 +1,7 @@
+import {test} from '@playwright/test'
+
+test("Prokabbadi Fixture using single x path", async ({page}) => {
+    await page.goto('https://www.prokabaddi.com/schedule-fixtures-results?tab=recent')
+    console.log(await page.locator('(//div/p[@class="match-count"])[1] | (//div/p[@class="team-name"])[1] | (//div/p[@class="score"])[1] | (//div/p[@class="score"])[2] | (//div/p[@class="team-name"])[2] | (//div/p[@class="match-place"])[1]').allTextContents())
+    
+})
